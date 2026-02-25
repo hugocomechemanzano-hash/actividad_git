7cbb4e67d9f1dde52edda6d95e0050146b0114ff
hugocomechemanzano-hash <hugocomechemanzano@estudiante.fjaverianas.com>
2026-02-25 13:40:20 +0100

    Fix main method and add student input prompts

--- AppAlumnoG.java
+++ AppAlumnoG.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
-
 public class AppAlumnoG {
 
-	public static void main
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.tn);
+	
+		System.out.println(" --. Registro de Alumno ...
+		System.out.print("Introduce el nonbre: ");
+		String nombre = sc.nextLine();
+		
+		System.out.print("Introduce el teléfono: ");
+		String telefono = sc.nextLine();
+		
+		System.out.print("Introduce la edad: ");
+		int edad = sc.nextInt();
+		
+		// Creamos el objeto Alumno (las notas se generan solas en el construc$
+		AlumnoG alumno = new AlumnoG(nombre, telefono, edad);
 }
